d1e0de7b4317882428fb67a80198c6aa4e948891
Vuong Thang <thangdv@blockchainlabs.asia>
2020-06-23 17:21:46 +0700

    remove code

--- app/feature/tracking-voting/tracking-voting.controller.js
+++ app/feature/tracking-voting/tracking-voting.controller.js
@@ -2,37 +2,19 @@ const logger = require("app/lib/logger");
 const config = require("app/config");
 const StakingAPI = require("app/lib/staking-api")
 
-module.exports = {
-  track: async (req, res, next) => {
-    try {
-      let result = await StakingAPI.trackingVote({
-        tx_id: req.body.tx_id,
-        voter_address: req.body.voter_address,
-        memo: req.body.memo,
-        type: req.body.type,
-        amount: req.body.amount
-      });
-      return res.status(result.httpCode).send(result.data);
-    }
-    catch (err) {
-      logger.error("tracking voting fail:", err);
-      next(err);
-    }
-  },
-  track3rd: async (req, res, next) => {
-    try {
-      let result = await StakingAPI.trackingVote3rd({
-        tx_id: req.body.tx_id,
-        voter_address: req.body.voter_address,
-        memo: req.body.memo,
-        type: req.body.type,
-        amount: req.body.amount
-      });
-      return res.status(result.httpCode).send(result.data);
-    }
-    catch (err) {
-      logger.error("tracking voting 3rd party fail:", err);
-      next(err);
-    }
+module.exports = async (req, res, next) => {
+  try {
+    let result = await StakingAPI.trackingVote({
+      tx_id: req.body.tx_id,
+      voter_address: req.body.voter_address,
+      memo: req.body.memo,
+      type: req.body.type,
+      amount: req.body.amount
+    });
+    return res.status(result.httpCode).send(result.data);
+  }
+  catch (err) {
+    logger.error("tracking voting fail:", err);
+    next(err);
   }
 }
--- app/feature/tracking-voting/tracking-voting.route.js
+++ app/feature/tracking-voting/tracking-voting.route.js
@@ -6,12 +6,7 @@ const route = express.Router();
 
 route.post("/voting",
   validator(requestSchema),
-  controller.track
-)
-
-route.post("/voting/3rd",
-  validator(requestSchema),
-  controller.track3rd
+  controller
 )
 
 
@@ -22,57 +17,6 @@ route.post("/voting/3rd",
  * /api/v1/voting:
  *   post:
  *     summary: tracking voting
- *     tags:
- *       - Voting
- *     description: Voting
- *     parameters:
- *       - in: body
- *         name: data
- *         description: Data for login.
- *         schema:
- *            type: object
- *            required: 
- *            example: 
- *               {
-                        "tx_id":"3f76680510bcca07e7e011dcc1effb079d1d0a34",
-                        "voter_address":"cosmos18vwj2myc5l7taspq2nrmscldq7gzuxlv9m586h",
-                        "memo":"Infinito:ATOM",
-                        "type":"DELEGATE|UNDELEGATE",
-                        "amount":100
-                  } 
- *     produces:
- *       - application/json
- *     responses:
- *       200:
- *         description: Ok 
- *         examples:
- *           application/json:
- *             {
- *                 "data": true
- *             }
- *       400:
- *         description: Error
- *         schema:
- *           $ref: '#/definitions/400'
- *       401:
- *         description: Error
- *         schema:
- *           $ref: '#/definitions/401'
- *       404:
- *         description: Error
- *         schema:
- *           $ref: '#/definitions/404'
- *       500:
- *         description: Error
- *         schema:
- *           $ref: '#/definitions/500'
- */
-
- /**
- * @swagger
- * /api/v1/voting/3rd:
- *   post:
- *     summary: tracking voting for 3rd party
  *     tags:
  *       - Voting
  *     description: Voting
