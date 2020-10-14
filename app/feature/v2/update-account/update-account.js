64d761a614a67141412f1d71ce92871ab4936ce7
Unknown <huyht@blockchainlabs.asia>
2020-10-14 18:12:19 +0700

    change folder name
    change folder name

--- app/feature/v2/update-account/update-account.js
+++ app/feature/v2/update-account/update-account.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const controller = require('./account.controller');
+const controller = require('./update-account.controller');
 const authenticate = require('app/middleware/authenticate.middleware');
 
 const router = express.Router();
