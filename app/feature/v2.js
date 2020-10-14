1dc66508aa3920e8e12e5da3ffedfa3ac92e42c9
Unknown <huyht@blockchainlabs.asia>
2020-10-14 18:12:19 +0700

    change folder name
    change folder name

--- app/feature/v2.js
+++ app/feature/v2.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.use(require('./v2/authentication/authentication.route'));
-router.use(require('./v2/accounts/member-info.route'));
-router.use(require('./v2/account/account.route'));
+router.use(require('./v2/get-account/member-info.route'));
+router.use(require('./v2/update-account/update-account'));
 
 module.exports = router;
