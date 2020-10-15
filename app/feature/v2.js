87d6643dbe4f92b6c713d3bfcf8bdc98c79919cf
Pham Ba Luan <luanpb@cardano-labo.com>
2020-10-15 15:12:40 +0700

    Api check email exist

--- app/feature/v2.js
+++ app/feature/v2.js
@@ -4,5 +4,6 @@ const router = express.Router();
 router.use(require('./v2/authentication/authentication.route'));
 router.use(require('./v2/get-account/member-info.route'));
 router.use(require('./v2/update-account/update-account.route'));
+router.use(require('./v2/get-account/check-email.route'));
 
 module.exports = router;
