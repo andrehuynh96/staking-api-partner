4807aea509f0743bc253d33f153a888816d0deb3
Pham Ba Luan <luanpb@cardano-labo.com>
2020-10-15 15:14:55 +0700

    Edit file .route

--- app/feature/v2.js
+++ app/feature/v2.js
@@ -3,6 +3,6 @@ const router = express.Router();
 
 router.use(require('./v2/authentication/authentication.route'));
 router.use(require('./v2/get-account/member-info.route'));
-router.use(require('./v2/update-account/update-account'));
+router.use(require('./v2/update-account/update-account.route'));
 
 module.exports = router;
