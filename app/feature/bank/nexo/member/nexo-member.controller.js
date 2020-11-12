402adcded1a6d109d2b356cc3a38f0ec5742087d
ducTD <ductd@blockchainlabs.asia>
2020-11-12 10:22:53 +0700

    clone get account - balance nexo account from web

--- app/feature/bank/nexo/member/nexo-member.controller.js
+++ app/feature/bank/nexo/member/nexo-member.controller.js
@@ -2,7 +2,8 @@ const logger = require('app/lib/logger');
 const BankFactory = require('app/service/banking/factory');
 const BankProvider = require('app/service/banking/provider');
 const NexoMember = require('app/model/wallet').nexo_members;
-
+const balanceMapper = require('./nexo-balance.response-schema');
+const mapper = require('./nexo-member.response-schema');
 module.exports = {
   recoveryRequest: async (req, res, next) => {
     try {
@@ -21,7 +22,7 @@ module.exports = {
         return res.badRequest(result.error.message, "NEXO_ERROR");
       return res.ok(true);
     } catch (err) {
-      logger.error('request recovery nexo account fail:', err);
+      logger[err.canLogAxiosError ? 'error' : 'info']('request recovery nexo accouont fail:', err);
       next(err);
     }
   },
@@ -56,7 +57,56 @@ module.exports = {
         });
       return res.ok(true);
     } catch (err) {
-      logger.error('verify recovery nexo account code fail:', err);
+      logger[err.canLogAxiosError ? 'error' : 'info']('verify recovery nexo account code fail:', err);
+      next(err);
+    }
+  },
+  getAccount: async (req, res, next) => {
+    try {
+      let { params: { device_code }, user } = req;
+      let where = {};
+      if (device_code) {
+        where.device_code = device_code
+      }
+      if (user && user.id) {
+        where.member_id = user.id
+      }
+      let account = await NexoMember.findOne({
+        where: where
+      });
+      if (!account)
+        return res.badRequest(res.__("NEXO_MEMBER_NOT_EXISTED"), "NEXO_MEMBER_NOT_EXISTED");
+      return res.ok(mapper(account));
+    } catch (err) {
+      logger[err.canLogAxiosError ? 'error' : 'info']('get nexo account fail:', err);
+      next(err);
+    }
+  },
+  getBalance: async (req, res, next) => {
+    try {
+      let { params: { device_code }, user } = req;
+      let where = {};
+      if (device_code) {
+        where.device_code = device_code
+      }
+      if (user && user.id) {
+        where.member_id = user.id
+      }
+      let account = await NexoMember.findOne({
+        where: where
+      });
+      if (!account)
+        return res.badRequest(res.__("NEXO_MEMBER_NOT_EXISTED"), "NEXO_MEMBER_NOT_EXISTED");
+      const Service = BankFactory.create(BankProvider.Nexo, {});
+      const result = await Service.getBalance({
+        nexo_id: account.nexo_id,
+        secret: account.user_secret
+      });
+      if (result.error)
+        return res.badRequest(result.error.message, "NEXO_ERROR");
+      return res.ok(balanceMapper(result))
+    } catch (err) {
+      logger[err.canLogAxiosError ? 'error' : 'info']('get balance by nexo account fail:', err);
       next(err);
     }
   }
