a0f409a0abc87d7a68d8f106ac97f6dcab572287
huyht-ibl <huyht@blockchainlabs.asia>
2020-11-13 11:32:09 +0700

    fix format

--- app/feature/get-asset/get-asset.controller.js
+++ app/feature/get-asset/get-asset.controller.js
@@ -259,9 +259,9 @@ function _getDateFilter(dateType, columnName) {
       query = `CONCAT(
             DATE_PART('YEAR', ${columnName}),
             '-',
-            to_char(DATE_PART('MONTH', ${columnName}),'00'), 
+            TRIM(to_char(DATE_PART('MONTH', ${columnName}),'00')), 
             '-',
-            to_char(DATE_PART('DAY', ${columnName}),'00'), 
+            TRIM(to_char(DATE_PART('DAY', ${columnName}),'00')), 
             ' ',
             DATE_PART('HOUR', ${columnName}),
             ':00')`;
@@ -272,9 +272,9 @@ function _getDateFilter(dateType, columnName) {
       query = `CONCAT(
                 DATE_PART('YEAR', ${columnName}),
                 '-',
-                to_char(DATE_PART('MONTH', ${columnName}),'00'), 
+                TRIM(to_char(DATE_PART('MONTH', ${columnName}),'00')), 
                 '-',
-                to_char(DATE_PART('DAY', ${columnName}),'00'))`;
+                TRIM(to_char(DATE_PART('DAY', ${columnName}),'00')))`;
       break;
 
     case 'YEAR':
