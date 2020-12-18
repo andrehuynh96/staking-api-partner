1a1f413a074d09527830af6573408651e164e5a7
huyht-ibl <huyht@blockchainlabs.asia>
2020-12-18 13:51:02 +0700

    redirect_to_fail_url

--- app/feature/fiat/transaction/transaction.route.js
+++ app/feature/fiat/transaction/transaction.route.js
@@ -215,6 +215,7 @@ module.exports = router;
  *            - dest_currency
  *            - dest_address
  *            - payment_method
+ *            - device_code
  *            example:
  *               {
                         "source_currency":"USD",
@@ -223,7 +224,8 @@ module.exports = router;
                         "dest_address": "moTXHK5dfgT62Y8XMM6RxRAVV8ojmofAnR",
                         "payment_method":"debit-card",
                         "redirect_url": "https://www.google.com",
-                        "failure_redirect_url": "https://www.google.com"
+                        "failure_redirect_url": "https://www.google.com",
+                        "device_code":"EFVGYHGFDewrF76&cQPLSwedftL""
                   }
  *     produces:
  *       - application/json
