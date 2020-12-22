ec720f29021fd205adb0f42a161175a3c380d580
huyht-ibl <huyht@blockchainlabs.asia>
2020-12-22 09:27:54 +0700

    hotfix get price

--- app/feature/feerate/feerate.response-schema.js
+++ app/feature/feerate/feerate.response-schema.js
@@ -15,7 +15,7 @@ const destObject = {
     id: 'id',
     name: 'name',
     symbol: 'symbol',
-    high: 'hig',
+    high: 'high',
     medium: 'medium',
     low: 'low',
     createdAt: 'created_at',
@@ -31,6 +31,7 @@ module.exports = srcObject => {
     }
   }
   else {
-    return objectMapper(srcObject, destObject.single);
+    let result = objectMapper(srcObject, destObject.single);
+    result.hig = result.high;
   }
 }; 
