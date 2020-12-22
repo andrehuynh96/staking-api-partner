2afd4ac9e9ccc6b1ba198902c8ff837309395222
myhn <myhn@blockchainlabs.asia>
2020-12-22 09:57:51 +0700

    fix search feerate list

--- app/feature/feerate/feerate.controller.js
+++ app/feature/feerate/feerate.controller.js
@@ -1,17 +1,19 @@
 const logger = require('app/lib/logger');
 const Feerates = require('app/model/wallet').feerates;
 const mapper = require('./feerate.response-schema');
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op;
 
 module.exports = {
   get: async (req, res, next) => {
     try {
-      let { symbol } = req.params
+      let { symbol } = req.params;
       let coin = await Feerates.findOne({
         where: {
-          symbol : symbol.toUpperCase()
+          symbol: symbol.toUpperCase()
         },
         raw: true
-      })
+      });
       return res.ok(mapper(coin));
     }
     catch (err) {
@@ -21,8 +23,8 @@ module.exports = {
   },
 
   getAll: async (req, res, next) => {
-    try{
-      let { query: { search } } = req
+    try {
+      let { query: { search } } = req;
       let limit = parseInt(req.query.limit) || 10;
       let offset = req.query.offset ? parseInt(req.query.offset) : 0;
       let where = {};
@@ -34,7 +36,7 @@ module.exports = {
           {
             name: { [Op.iLike]: `%${search}%` }
           }]
-        }
+        };
       }
       const { count: total, rows: items } = await Feerates.findAndCountAll({ limit, offset, where: where, order: [['symbol', 'ASC']] });
       return res.ok({
@@ -49,4 +51,4 @@ module.exports = {
       next(err);
     }
   }
-}
+};
