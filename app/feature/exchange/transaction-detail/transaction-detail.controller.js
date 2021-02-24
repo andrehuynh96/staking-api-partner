13eb0025296902ffd25d495a9b4df8a0f316f5da
Huy <huyht@blockchainlabs.asia>
2021-02-24 10:21:33 +0700

    fix get exchange transaction detail

--- app/feature/exchange/transaction-detail/transaction-detail.controller.js
+++ app/feature/exchange/transaction-detail/transaction-detail.controller.js
@@ -1,20 +1,25 @@
 const logger = require('app/lib/logger');
 const ExchangeTransaction = require('app/model/wallet').exchange_transactions;
 const Mapper = require("app/feature/response-schema/exchange/transaction.response-schema");
+const uuidvalidate = require('uuid-validate');
 const Sequelize = require('sequelize');
 const Op = Sequelize.Op;
 
 module.exports = async (req, res, next) => {
   try {
     logger.info('transaction::detail');
-    const where = {
-      [Op.or]: [{
+    let where = {};
+    if (uuidvalidate(req.params.id)) {
+      where = {
         id: req.params.id
-      },
-      {
+      };
+    }
+    else {
+      where = {
         transaction_id: req.params.id
-      }]
-    };
+      };
+    }
+
     const transaction = await ExchangeTransaction.findOne({ where: where });
     if (!transaction) {
       return res.badRequest(res.__("TRANSACTION_NOT_FOUND"), "TRANSACTION_NOT_FOUND", {
